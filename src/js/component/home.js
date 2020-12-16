1449b5f2c53d0f050c1997316826a99f25713112
felix-oss <uv.mhjl@gmail.com>
2020-12-16 20:57:06 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,13 +1,59 @@
 import React from "react";
+import "./home.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			
-		</div>
-	);
+export class Home extends React.Component {
+	constructor() {
+		super();
+	}
+	render() {
+		return (
+			<div classNameName="container">
+				<nav
+					className="navbar navbar-light"
+					style={{
+						borderBottomStyle: "solid",
+						borderWidth: "1px",
+						borderColor: "rgb(50, 50, 50)"
+					}}>
+					<div className="container-fluid">
+						<form className="d-flex">
+							<input
+								className="form-control me-2"
+								type="search"
+								placeholder="Search"
+								aria-label="Search"
+								style={{ borderRadius: "5em" }}
+							/>
+						</form>
+					</div>
+				</nav>
+				<ol>
+					<li
+						style={{
+							borderBottomStyle: "solid",
+							borderWidth: "1px",
+							borderColor: "rgb(50, 50, 50)"
+						}}>
+						cow
+					</li>
+					<li
+						style={{
+							borderBottomStyle: "solid",
+							borderWidth: "1px",
+							borderColor: "rgb(50, 50, 50)"
+						}}>
+						pie
+					</li>
+					<li
+						style={{
+							borderBottomStyle: "solid",
+							borderWidth: "1px",
+							borderColor: "rgb(50, 50, 50)"
+						}}>
+						pot
+					</li>
+				</ol>
+			</div>
+		);
+	}
 }
